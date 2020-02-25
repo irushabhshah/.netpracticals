fd97e5a0f0f4fd87c9486d6978ce3f5778ffc645
Rushabh Shah <shah96rushabh@gmail.com>
2020-02-25 13:37:32 +0530

    [Practical-4]

---
+++ dotNet_Lab/Practical-1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Practical_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PatternOne();
+            PatternTwo();
+            PatternThree();
+        }
+
+        private static void PatternOne()
+        {
+            Console.WriteLine("Patter 1 Start");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine("Patter 1 End");
+        }
+        private static void PatternTwo()
+        {
+            Console.WriteLine("Patter 2 Start");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine("Patter 2 End");
+        }
+        private static void PatternThree()
+        {
+            Console.WriteLine("Patter 3 Start");
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 5; j >= i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <=i; k++) {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine("Patter 3 End");
+        }
+    }
+}
---
+++ dotNet_Lab/Practical-4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practical_4
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myclass = new MyClass();
+            String infix = "(x-y)*(w+z)";
+            String postfix;
+            myclass.Infixtopostfix(ref infix, out postfix);
+            Console.WriteLine("infix string : " + infix);
+            Console.WriteLine("postfix string is : " + postfix);
+        }
+    }
+}
